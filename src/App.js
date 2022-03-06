ffa90459eaf0049ff0c07a2d69f1d7c556c6cc7b
rharison <rharison.abreu@gmail.com>
2022-03-06 15:22:06 -0300

    Criando container dos cards.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import ContainerCards from './Components/Cards/ContainerCards';
 import CarrinhoBody from './Components/Carrinho/CarrinhoBody';
 
 function App() {
   return (
     <div className="bodyPage">
       <CarrinhoBody></CarrinhoBody>
+      <ContainerCards></ContainerCards>
     </div>
   );
 }
---
+++ src/Components/Cards/ContainerCards.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './ContainerCards.module.css';
+
+const ContainerCards = () => {
+  return <div className={styles.cards}></div>;
+};
+
+export default ContainerCards;
