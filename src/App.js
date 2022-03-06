feac2083047f6fce2bf237fffa14e64e1dcbc4b0
rharison <rharison.abreu@gmail.com>
2022-03-06 15:34:53 -0300

    Adicionando componente de navegação por grupos.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import ContainerCards from './Components/Cards/ContainerCards';
 import CarrinhoBody from './Components/Carrinho/CarrinhoBody';
+import GruposNavegacao from './Components/Navegacao/GruposNavegacao';
 
 function App() {
   return (
     <div className="bodyPage">
       <CarrinhoBody></CarrinhoBody>
+      <GruposNavegacao></GruposNavegacao>
       <ContainerCards></ContainerCards>
     </div>
   );
---
+++ src/Components/Navegacao/GruposNavegacao.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './GruposNavegacao.module.css';
+
+const GruposNavegacao = () => {
+  return <div className={`${styles.shadow} ${styles.dayPasseio}`}></div>;
+};
+
+export default GruposNavegacao;
